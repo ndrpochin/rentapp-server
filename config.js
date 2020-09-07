bb9f72c46a4246feb3cf1f01cafde4e8500a2777
Nadir Jaramillo <nadir.jaramillorosasa@gmail.com>
2020-09-07 19:33:58 -0300

    actualizado config host

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
       //host: process.env.APP_HOST,
-      host: 'http://localhost', //Funciona
+      host: 'https://team5-rentapp.herokuapp.com', //Funciona
       port: process.env.PORT || 3000,
       MONGODB: process.env.MONGODB_URI || 'mongodb+srv://patricio:grupo5@cluster0.ibf0e.mongodb.net/rentappDB?retryWrites=true&w=majority',
     }
